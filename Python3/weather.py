3c9ae3e2b64c2d5145d90557f2a26ace35008dfa
zzygit <zzygit@gmail.com>
2016-06-15 09:00:58 +0800

    Add Weather.py

---
+++ Python3/weather.py
@@ -0,0 +1,8 @@
+import urllib.request
+import json
+
+class weather:
+    def getWeather(self):
+        url = 'http://apis.baidu.com/thinkpage/weather_api/suggestion?location=ip&language=en&unit=c&start=0&days=3'
+        head
+        req=request.Request
